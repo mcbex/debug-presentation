db4942a59724eacbb8afbd182539b40a01fe7cb8
Beckwith <beckwmi2@localhost.na.novartis.net>
2015-01-22 01:40:18 -0500

    slightly less horrible + some bug examples

--- js/views/editorView.js
+++ js/views/editorView.js
@@ -2,6 +2,10 @@ define(['backbone'], function(Backbone) {
 
     var EditorView = Backbone.View.extend({
 
+        events: {
+            'click td': 'changeColor'
+        },
+
         pixelSize: 4,
 
         initialize: function() {
@@ -26,42 +30,70 @@ define(['backbone'], function(Backbone) {
             var width = this.model.get('context').canvas.width,
                 height = this.model.get('context').canvas.height,
                 data = this.model.get('context').getImageData(0, 0, width, height),
-                $table = $('<table id="horrible-table">'), $row, $cell;
+                $table = $('<table id="horrible-table">'), html = '<tr>', view = this;
 
-            this.$el.find('.editor').append($table);
 
             for (var i = 0, l = data.data.length; i < l; i += 4) {
-                if (!(i % (width * 4))) {
-                    $row = $('<tr>');
-                    $table.append($row);
+                if (i != 0 && !(i % (width * 4))) {
+                    html += '</tr><tr>';
+                    html += '<td style="background-color: rgb(' +
+                        [data.data[i], data.data[i + 1], data.data[i + 2]].join(',').toString() +
+                        ')" class="' + [data.data[i], data.data[i + 1], data.data[i + 2]].join('_').toString() +
+                        '"></td>';
+                } else {
+                    html += '<td style="background-color: rgb(' +
+                        [data.data[i], data.data[i + 1], data.data[i + 2]].join(',').toString() +
+                        ')" class="' + [data.data[i], data.data[i + 1], data.data[i + 2]].join('_').toString() +
+                        '"></td>';
                 }
-                $cell = $('<td>');
-                $row.append($cell);
-                $cell.css({
-                    'background-color': 'rgba(' + data.data[i] + ',' + data.data[i + 1] + ',' + data.data[i + 2] + ',' + data.data[i + 3] + ')'
-                });
-                $cell.width(this.pixelSize);
-                $cell.height(this.pixelSize);
             }
 
-            $('td').on('click', function(e) {
-                var color = $(e.currentTarget).css('background-color');
+            html += '</tr>';
 
-                _.forEach($('td'), function(t) {
-                    if ($(t).css('background-color') == color) {
-                        $(t).css({ 'background-color': 'rgba(250,0,0,1)' });
-                    }
-                });
+            $table.html(html);
+
+            $table.find('td').css({
+                width: view.pixelSize,
+                height: view.pixelSize
             });
+
+            view.$el.find('.editor').append($table);
+
+//            $('td').on('click', function(e) {
+//                view.changeColor(e);
+//            });
 
             console.log(new Date().getTime());
         },
 
         clearTable: function() {
-            // makes memory leak
-            // this.$el.find('#horrible-table').detach();
             this.$el.find('.editor').empty();
-        }
+        },
+
+        rgbToString: function(colorArray) {
+            return _.reduce(colorArray, function(memo, c) {
+                memo.push(('000' + c).slice(-3));
+                return memo;
+            }, []).join('_').toString();
+        },
+
+        changeColor: function(e) {
+            var cls = $(e.currentTarget).attr('class');
+
+            $('.' + cls).css({
+                'background-color': 'rgb(255,0,0)'
+            }).removeClass(cls).addClass('255_0_0');
+        },
+
+//        changeColor: function(e) {
+//            var color = $(e.currentTarget).css('background-color');
+//
+//            _.forEach($('td'), function(t) {
+//                if ($(t).css('background-color') == color) {
+//                    $(t).css('background-color', 'rgb(255,0,0)');
+//                }
+//            })
+//        }
 
     });
 
