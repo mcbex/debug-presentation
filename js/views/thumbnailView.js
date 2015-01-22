f8b144a148569d89097d52bf496296d7ed862e0a
Beckwith <beckwmi2@localhost.na.novartis.net>
2015-01-21 21:39:56 -0500

    configurable view + load big image on select

--- js/views/thumbnailView.js
+++ js/views/thumbnailView.js
@@ -6,40 +6,63 @@ define(['backbone', 'async'], function(Backbone, async) {
 
         id: 'thumbnails',
 
-        images: ['/thumbs/daisies.jpg'],
+        images: ['daisies', 'abstract'],
+
+        events: {
+            'click img': 'selectImage'
+        },
 
         initialize: function(options) {
+            this.width = options.attributes.width || 50;
             this.render();
         },
 
         render: function() {
-            var view = this;
-            // add custom rendered event to be triggered
 
             // bug - images will have 0 height when the canvas is drawn
 //            _.forEach(this.images, function(i) {
 //                view.$el.append('<img src="' + i + '"/>');
 //            });
             // draw chrome here
+            this.$el.html('<div class="image-container">');
             this.loadImages();
         },
 
         // put this in the model
-        // fetch little images then on click get bigger one and render
         loadImages: function() {
-            var view = this, img;
+            var view = this, $img;
 
             async.each(this.images, function(i, cb) {
-                img = $('<img src="' + i + '"/>');
-                img.load(function() {
-                    view.$el.append(this);
-                    cb();
-                });
+                $img = $('<img class="' + i + '" src="thumbs/' + i + '.jpg"/>')
+                    .load(function() {
+                        view.$el.append($(this).width(view.width));
+                        cb();
+                    });
             }, function(err) {
                 // no errors yet
                 console.log(new Date().getTime());
                 err ? view.trigger('renderError', err) : view.trigger('rendered');
             });
+        },
+
+        selectImage: function(e) {
+            var imageName = e.currentTarget.className, view = this,
+                image = view.$el.find('.image-container').find('img.' + imageName);
+
+            if (image.length) {
+                view.selectedImage = image;
+                view.trigger('imageSelected');
+            } else {
+                this.selectedImage = $('<img class="' + imageName + '" src="images/' + imageName + '.jpg"/>')
+                    .load(function() {
+                        $(this).appendTo(view.$el.find('.image-container'));
+                        view.trigger('imageSelected');
+                    });
+            }
+        },
+
+        getSelectedImage: function() {
+            return this.selectedImage;
         }
 
     });
